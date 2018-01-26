5b73179103a3209c811823c2453d2f9f4681a9e0
marcelox3010 <marcelo@gomesgadelha.com>
2018-01-26 13:11:31 -0200

    EXEMPLO CPF VALIDAR

--- javascript/MascaraCPF.js
+++ javascript/MascaraCPF.js
@@ -1,6 +1,10 @@
 /*
 
     MÁSCARA PARA CPF
+    
+    EXEMPLO DE COMO CHAMAR:
+    <input type='text' name='cpf1' id='id_cpf1'  maxlength='14'
+            						onkeypress='javascript:this.value = mascaraCPF(this.value)'>
 
 */
 
@@ -10,6 +14,6 @@ function mascaraCPF(v){
           v=v.replace(/(\d{3})(\d)/,"$1.$2");
           v=v.replace(/(\d{3})(\d)/,"$1.$2");                                                                               
           v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2");                             
-          return v
+          return v;
          
 }
