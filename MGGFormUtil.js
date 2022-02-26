425478783c497c0c720870a4ef5ce76e51b6c522
Marcelo Gadelha <marcelox3010@gmail.com>
2022-02-26 10:12:55 -0300

    ajuste na função cnpj

--- MGGFormUtil.js
+++ MGGFormUtil.js
@@ -128,42 +128,39 @@ function validaCNPJ(v){
         }
 
     }
-    if (!digitos_iguais){
 
-        tamanho = cnpj.length - 2
-        numeros = cnpj.substring(0,tamanho);
-        digitos = cnpj.substring(tamanho);
-        soma = 0;
-        pos = tamanho - 7;
-        for (i = tamanho; i >= 1; i--)
-        {
-            soma += numeros.charAt(tamanho - i) * pos--;
-            if (pos < 2)
-                pos = 9;
-        }
+    if(digitos_iguais) return false; 
 
-        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-        if (resultado != digitos.charAt(0)) return false;
-
-        tamanho = tamanho + 1;
-        numeros = cnpj.substring(0,tamanho);
-        soma = 0;
-        pos = tamanho - 7;
-        for (i = tamanho; i >= 1; i--){
-
-            soma += numeros.charAt(tamanho - i) * pos--;
-            if (pos < 2)
+    tamanho = cnpj.length - 2
+    numeros = cnpj.substring(0,tamanho);
+    digitos = cnpj.substring(tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--)
+    {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2)
             pos = 9;
-
-        }
-        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-        if (resultado != digitos.charAt(1)) return false;
-
-        return true;
-
     }
 
-    else return false;
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(0)) return false;
 
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0,tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--){
+
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2)
+        pos = 9;
+
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(1)) return false;
+
+    return true;
+    
 }
     
